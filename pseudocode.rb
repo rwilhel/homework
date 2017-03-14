42e47eaab9a41c112ad32398b03ff3b566a8dd2b
rwilhel <rwilhel@gmail.com>
2017-03-14 09:09:30 -0600

    first commit

---
+++ pseudocode.rb
@@ -0,0 +1,8 @@
+The program is an imitation of a Customer Service Representative with a poor phone connection.
+The CSR/program opens/answers the phone with "HELLO, THIS IS A GROCERY STORE!"
+If the user doesn't say/input anything & just hits enter, then the operator AKA program responds with "HELLO?!"
+If the user asks a question too quietly/with lower-case letters, then the program responds with "I AM HAVING A HARD TIME HEARING YOU."
+If the user asks a question loudly/in all upper-case, then the program responds with "NO, THIS IS NOT A PET STORE"
+If the user says "GOODBYE!" once, then the program says "ANYTHING ELSE I CAN HELP WITH?"
+If the user says "GOODBYE!" a second time, then the program says "THANK YOU FOR CALLING!" and ends the call/exits the program.
+
