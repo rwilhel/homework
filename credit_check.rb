a9ebe607eab940f87475ff9146e8447e553a95b9
rwilhel <rwilhel@gmail.com>
2017-03-18 14:00:03 -0600

    First edit to credit_check.rb

---
+++ credit_check.rb
@@ -0,0 +1,12 @@
+card_number = "4929735477250543"
+
+full_number = card_number + x
+
+
+valid = false
+
+# Your Luhn Algorithm Here
+
+# Output
+## If it is valid, print "The number is valid!"
+## If it is invalid, print "The number is invalid!"
